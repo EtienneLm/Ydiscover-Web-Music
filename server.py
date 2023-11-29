62056d660640e45ef7e5bf69895ebe15d8ddad5d
letienne <etienne.lemee@ynov.com>
2023-11-29 11:05:07 +0100

    added home route and changed page icon name

--- server.py
+++ server.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('main.html')
+    return render_template('home.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 # @app.route('/test')
 # def hello():
