cd2239e52b27bfc28134b815aa8501301a66f3e7
Godlonx <godlonx44@gmail.com>
2024-01-24 08:25:00 +0100

    :rocket: | usable version of api script

--- api.py
+++ api.py
@@ -1,32 +1,50 @@
-import requests
+import requests as req
+import json
 
 clientId = "ac2895073db74d878d7f432feb920e66"
 clientSecret = "3af3cd792c674420b0cbbfc41c752481"
 
-auth_url = 'https://accounts.spotify.com/api/token'
-
-data = {
-    'grant_type': 'client_credentials',
-    'client_id': clientId,
-    'client_secret': clientSecret,
-}
-
-auth_response = requests.post(auth_url, data=data)
-if auth_response:
-    access_token = auth_response.json().get('access_token')
-
-    base_url = 'https://api.spotify.com/v1/'
-    headers = {
-        'Authorization': 'Bearer {}'.format(access_token)
-    }
-    featured_playlists_endpoint = 'browse/featured-playlists/?limit=10'
-    featured_playlists_url = ''.join([base_url,featured_playlists_endpoint])
-    response = requests.get(featured_playlists_url,headers=headers)
-    if response:
-        playlists = response.json().get('playlists').get('items')
-        playlist_ids = set()
-        for pl in playlists:
-            playlist_id = pl.get('name')
-            playlist_ids.add(playlist_id)
-        print(playlist_ids)
         
+def getToken():
+    auth_url = 'https://accounts.spotify.com/api/token'
+    data = {'grant_type': 'client_credentials','client_id': clientId,'client_secret': clientSecret}
+    res = req.post(auth_url, data=data)
+    if res.status_code == 200:
+        return res.json().get('access_token')
+
+def getPlaylist(self, genres, limit=1):
+    spotifyPath = "https://api.spotify.com/v1/search"
+    # target_danceability=0.9
+
+    request = f'{spotifyPath}?q=genre%3A{genres}&type=track&limit={limit}'
+
+    res = req.get(request, headers={"Content-Type":"application/json", "Authorization":f"Bearer {self.accessToken}"})
+
+    data = json.loads(res.text)
+    allTracks = []
+    for track in data['tracks']['items']:
+        allTracks.append(track['external_urls']['spotify'])
+    return allTracks
+
+def getAllEmbebs(self, urls):
+    embedUrls = []
+    for url in urls:
+        embedUrls.append(self.getEmbeb(url))
+    return embedUrls
+        
+def getEmbeb(self, url):
+    splitedUrl = url.split("/")
+    embedUrl = ""
+    for id, val in enumerate(splitedUrl):
+        embedUrl+=val
+        if val == 'open.spotify.com':
+            embedUrl += "/embed/"
+        elif id < len(splitedUrl)-1:
+            embedUrl += "/"
+    return embedUrl
+
+
+
+#<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6BK2krum9Qbli37FN6VqvL?utm_source=generator" 
+# width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; 
+# picture-in-picture" loading="lazy"></iframe>
